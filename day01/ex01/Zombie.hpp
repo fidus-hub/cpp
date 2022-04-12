693b1721496a00c5b238507eea40984029b2692c
Mohamed amine Grissen <mgrissen@e2r2p2.1337.ma>
2022-04-12 22:41:48 +0000

    ex01

---
+++ day01/ex01/Zombie.hpp
@@ -0,0 +1,22 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cctype>
+
+# define	string std::string
+# define	cout std::cout
+# define	cin std::cin
+# define	endl std::endl
+# define	setw std::setw
+# define	setfill std::setfill
+# define	left std::left
+# define 	getline std::getline
+
+class Zombie
+{
+
+};
+
+#endif
